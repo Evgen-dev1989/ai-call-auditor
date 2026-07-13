60829c05b598097e237c754a26c8b39455738ff6
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2026-07-13 19:17:01 +0300

    Add audio transcription function
    Implement transcribe_audio() function that converts audio files to text using the faster-whisper large model. The function leverages GPU acceleration (CUDA) with float16 precision and supports Russian language transcription, returning the combined text from all audio segments.

--- sound_to_text.py
+++ sound_to_text.py
@@ -2,3 +2,18 @@ import os
 import warnings
 from faster_whisper import WhisperModel
 
+def transcribe_audio(file_path):
+
+    local_model_path = "./whisper_model"
+    
+    model = WhisperModel("Systran/faster-whisper-large-v3", device="cuda", compute_type="float16")
+
+    print(f"satrt: {os.path.basename(file_path)}...")
+    
+    segments, info = model.transcribe(file_path, language="ru", beam_size=1)
+    
+    full_text = []
+    for segment in segments:
+        full_text.append(segment.text)
+        
+    return "".join(full_text)
